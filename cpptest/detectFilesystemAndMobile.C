8daf09d0430f9fa1bed841e8b8095f3e8bbe4699
Mitchell Waite <mitchellwaite@users.noreply.github.com>
2025-11-17 23:42:48 -0500

    Update detectFilesystemAndMobile.C

--- cpptest/detectFilesystemAndMobile.C
+++ cpptest/detectFilesystemAndMobile.C
@@ -43,19 +43,27 @@ void printSpareData(SMALL_BLOCK_SPARE_DATA spare)
    // Block ID is stored as a little endian value in the spare data
    uint16_t block_id = (((spare.BlockID0&0xF)<<8)+(spare.BlockID1));//le16toh(*(uint16_t *)&(spare.BlockID1));
    uint16_t fs_size = le16toh(*(uint16_t *)&(spare.FsSize1));
+   uint32_t fs_version = (spare.FsSequence3 << 24) + (spare.FsSequence2 << 16) + (spare.FsSequence1 << 8) + spare.FsSequence0;
+
 
    //printf("Block ID 1: 0x%x\n",spare.BlockID1);
    //printf("Block ID 0: 0x%x\n",spare.BlockID0);
 
    printf("-- spare data dump --\n");
+   
    printf("Block ID: 0x%x\n",block_id);
-   printf("Address of block: 0x%x\n",getPhysicalBlockAddress(block_id));
-   printf("FS sequence: 0x%x 0x%x 0x%x 0x%x\n",spare.FsSequence0,spare.FsSequence1,spare.FsSequence2,spare.FsSequence3);
+   printf("PBA: 0x%x\n",getPhysicalBlockAddress(block_id));
+   printf("LBA: 0x%x\n",getLogicalBlockAddress(block_id));
+
+   //printf("FS sequence: 0x%x 0x%x 0x%x 0x%x\n",spare.FsSequence0,spare.FsSequence1,spare.FsSequence2,spare.FsSequence3);
+   printf("File Version: %d\n", fs_version);
    //printf("Bad Block Indicator: 0x%x\n",spare.BadBlockIndicator);
    printf("File Size: 0x%x\n",fs_size);
    //printf("FS free page Count: 0x%x\n",spare.FsPageCount);
    printf("Used pages in block: 0x%x\n",0x20 - spare.FsFreePageCount);
+
    //printf("FS Unused: 0x%x 0x%x\n",spare.FsUnused0,spare.FsUnused1);
+
    printf("FS Block Type: 0x%x\n",spare.ECC3&0x3f); // bottom 6 bits of ECC3 is the block type
    //printf("ECC: 0x%x 0x%x 0x%x 0x%x\n",spare.ECC3&0xC0,spare.ECC2,spare.ECC1,spare.ECC0);
 }
@@ -82,47 +90,22 @@ int main(int argc, char * argv[])
 
    FILE * fd = fopen("testimg.bin","rb");
 
+   uint8_t mobileDatType = 0x0;
+
    for(int i = 0x0; i < 0x4000; i++)
    {
       // Seek to the spare data
       fseek(fd, getPhysicalBlockAddress(i) + 0x200, SEEK_SET);
       fread(&sprd,sizeof(SMALL_BLOCK_SPARE_DATA),1,fd);
 
-      switch(getMobileDatType(sprd))
-      {
-         case 0x31:
-            printf("Found MobileB in block %x\n",getLogicalBlockAddress(i));
-            break;
-         case 0x32:
-            printf("Found MobileC in block %x\n",getLogicalBlockAddress(i));
-            break;
-         case 0x33:
-            printf("Found MobileD in block %x\n",getLogicalBlockAddress(i));
-            break;
-         case 0x34:
-            printf("Found MobileE in block %x\n",getLogicalBlockAddress(i));
-            break;
-         case 0x35:
-            printf("Found MobileF in block %x\n",getLogicalBlockAddress(i));
-            break;
-         case 0x36:
-            printf("Found MobileG in block %x\n",getLogicalBlockAddress(i));
-            break;
-         case 0x37:
-            printf("Found MobileH in block %x\n",getLogicalBlockAddress(i));
-            break;
-         case 0x38:
-            printf("Found MobileI in block %x\n",getLogicalBlockAddress(i));
-            break;
-         case 0x39:
-            printf("Found MobileJ in block %x\n",getLogicalBlockAddress(i));
-            break;
-         default:
-            continue;
-      }
+      mobileDatType = getMobileDatType(sprd);
 
-      printSpareData(sprd);
-      printf("\n");
+      if(mobileDatType != 0x0)
+      {
+         printf("Found Mobile%c in block %x\n",mobileDatType + 0x11,i);
+         printSpareData(sprd);
+         printf("\n");
+      }
    }
 
    fclose(fd);
